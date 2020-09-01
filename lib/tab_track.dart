7ac1695cf611b604005cd011e5a11fcfd3b601f3
Mark Leavitt <mark@markleavitt.com>
2020-09-01 08:48:29 -0700

    Google maps added to Track screen, basic fixed center for now.

--- lib/tab_track.dart
+++ lib/tab_track.dart
@@ -1,13 +1,31 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:geolocator/geolocator.dart';
 import 'race_data.dart';
+import 'package:provider/provider.dart';
 
-class TabTrack extends StatelessWidget {
+class TabTrack extends StatefulWidget {
   @override
+  _TabTrackState createState() => _TabTrackState();
+}
+
+class _TabTrackState extends State<TabTrack> {
+  GoogleMapController mapController;
+  final LatLng _center = const LatLng(45.521563, -122.677433);
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text('tracker goes here');
+    return GoogleMap(
+      onMapCreated: _onMapCreated,
+      initialCameraPosition: CameraPosition(
+        target: _center,
+        zoom: 11.0,
+      ),
+    );
   }
 }
 
